9b57155b1b800c68c2255e94dd45d3a0b7bc598f
Prince Raj <prince.raj29042004@gmai.com>
2022-08-09 10:48:20 +0530

    Improved CHat screen

---
+++ lib/layouts.dart
@@ -0,0 +1,86 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+Icon suitableIcon(IconData icon, {Color? color}) {
+  return Icon(
+    icon,
+    color: color,
+    size: 16,
+  );
+}
+
+Widget newMessageIcon(int noOfMessages) {
+  return Container(
+    padding: EdgeInsets.all(2),
+    child: Stack(alignment: Alignment.center, children: [
+      Icon(
+        Icons.circle,
+        color: Colors.green,
+        size: 22,
+      ),
+      Text(
+        "$noOfMessages",
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 12,
+        ),
+      )
+    ]),
+  );
+}
+
+class DisplayPicture extends StatelessWidget {
+  const DisplayPicture({Key? key, required this.image}) : super(key: key);
+
+  final String image;
+
+  @override
+  Widget build(BuildContext context) {
+    Widget av = CircleAvatar(
+      radius: 25,
+      foregroundImage: NetworkImage(image),
+    );
+    return av;
+  }
+}
+
+class UpperText extends StatelessWidget {
+  const UpperText({Key? key, required this.text}) : super(key: key);
+
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      style: TextStyle(fontSize: 15.5, fontWeight: FontWeight.bold),
+    );
+  }
+}
+
+class BottomText extends StatelessWidget {
+  const BottomText({Key? key, required this.message, this.icon})
+      : super(key: key);
+
+  final String message;
+  final Icon? icon;
+
+  @override
+  Widget build(BuildContext context) {
+    double width = 0;
+    if (icon != null) {
+      width = 4;
+    }
+    return Row(
+      children: [
+        icon ?? SizedBox(),
+        SizedBox(
+          height: 14,
+          width: width,
+        ),
+        Text(message),
+      ],
+    );
+  }
+}
--- lib/screens/chats.dart
+++ lib/screens/chats.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
+import 'package:whatsapp_clone/layouts.dart';
 
 class ChatScreen extends StatefulWidget {
   const ChatScreen({Key? key}) : super(key: key);
@@ -17,17 +18,21 @@ class _ChatScreenState extends State<ChatScreen> {
         dp: "https://i.stack.imgur.com/F4eOi.jpg?s=64&g=1",
         name: "Prince Raj",
         message: "Haha yes!",
-        status: 0,
+        time: "Just now",
+        newMessage: true,
       ),
       Chat(
           dp: "https://thumbs.dreamstime.com/b/vector-illustration-avatar-dummy-logo-collection-image-icon-stock-isolated-object-set-symbol-web-137160339.jpg",
           name: "Ashish",
-          message: "Ok I'll do it."),
+          message: "Ok I'll do it.",
+          time: "4:20 am",
+          icon: suitableIcon(Icons.done)),
       Chat(
         dp: "https://c6oxm85c.cloudimg.io/cdno/n/q85/https://az617363.vo.msecnd.net/imgmodels/models/MD10002954/untit9521a9013a921a3bbb56d9fe8f5f9779_thumb3872f63f05c818fa90fb01d6a7d89cbe_thumb.jpg",
         name: "Paige",
         message: "Done!",
-        status: 2,
+        time: "29/04/22",
+        icon: suitableIcon(Icons.done_all, color: Colors.blue.shade400),
       ),
     ]);
   }
@@ -39,111 +44,64 @@ class Chat extends StatelessWidget {
       required this.dp,
       required this.name,
       required this.message,
-      this.status = 1})
+      required this.time,
+      this.icon,
+      this.newMessage = false})
       : super(key: key);
 
-  final String dp;
-  final String name;
-  final String message;
-  final int status;
+  final String dp, name, message, time;
+  final Icon? icon;
+  final bool newMessage;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.fromLTRB(10, 10, 5, 2),
-      child: Row(
-        children: [
-          DisplayPicture(
-            image: dp,
-          ),
-          Padding(
-            padding: const EdgeInsets.only(left: 10),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                ContactName(
-                  name: name,
-                ),
-                Message(
-                  message: message,
-                  status: status,
-                )
-              ],
+    return InkWell(
+      onTap: () => ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        content: Text("Not Implemented Yet!"),
+        duration: Duration(seconds: 1),
+      )),
+      child: Container(
+        margin: EdgeInsets.fromLTRB(10, 10, 10, 2),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            DisplayPicture(
+              image: dp,
             ),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class Message extends StatelessWidget {
-  const Message({Key? key, required this.message, this.status = 1})
-      : super(key: key);
-
-  final String message;
-  final int status;
-
-  Widget getIcon(status) {
-    if (status == 0) {
-      return Container();
-    } else if (status == 1) {
-      return Icon(
-        Icons.done,
-        size: 14,
-      );
-    } else {
-      return Icon(
-        Icons.done_all,
-        size: 14,
-        color: Colors.blue.shade400,
-      );
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      children: [
-        getIcon(status),
-        SizedBox(
-          height: 14,
-          width: 4,
+            Padding(
+              padding: const EdgeInsets.only(left: 10, top: 8),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  UpperText(
+                    text: name,
+                  ),
+                  BottomText(
+                    message: message,
+                    icon: icon,
+                  )
+                ],
+              ),
+            ),
+            Spacer(),
+            Padding(
+              padding: const EdgeInsets.only(top: 8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Text(
+                    time,
+                    style: TextStyle(
+                        fontSize: 11.3,
+                        color: newMessage ? Colors.green : null),
+                  ),
+                  newMessage ? newMessageIcon(1) : Container()
+                ],
+              ),
+            )
+          ],
         ),
-        Text(message),
-      ],
-    );
-  }
-}
-
-class ContactName extends StatelessWidget {
-  const ContactName({Key? key, required this.name}) : super(key: key);
-
-  final String name;
-
-  @override
-  Widget build(BuildContext context) {
-    return Text(
-      name,
-      style: TextStyle(fontSize: 15.5, fontWeight: FontWeight.bold),
-    );
-  }
-}
-
-class DisplayPicture extends StatelessWidget {
-  const DisplayPicture({Key? key, required this.image}) : super(key: key);
-
-  final String image;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: 49,
-      height: 49,
-      decoration: BoxDecoration(
-          shape: BoxShape.circle,
-          color: Colors.white,
-          image: DecorationImage(fit: BoxFit.fill, image: NetworkImage(image))),
+      ),
     );
   }
 }
